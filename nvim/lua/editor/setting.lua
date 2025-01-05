c82a183fa3fbe59d2da50324ce382e524551603a
hirohata <43802663+hitohata@users.noreply.github.com>
2025-01-05 14:39:59 -0800

    feat: updated the editor setting (#8)

---
+++ nvim/lua/editor/setting.lua
@@ -0,0 +1,11 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.syntax = "on"
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.encoding = "UTF-8"
+o.title = true
