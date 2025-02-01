5e4ca1d7f44742a5ae209a2c393c32c3d724a04b
hirohata <hirohatatro@gmail.com>
2025-02-01 10:31:59 -0800

    added repo link

---
+++ nvim/lua/plugins/repo-link.lua
@@ -0,0 +1,15 @@
+-- https://github.com/9seconds/repolink.nvim?tab=readme-ov-file
+return {
+	"9seconds/repolink.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	cmd = {
+		"RepoLink",
+	},
+
+	opts = {
+		-- your configuration goes here.
+		-- keep empty object if you are fine with defaults
+	},
+}
