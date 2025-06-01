e63a9f347803fcac80b51c57b168bb1b59f615b4
hirohata <hirohatatro@gmail.com>
2025-06-01 16:43:36 -0700

    feat: updated LSP settings

---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,25 @@
+return {
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"bash-language-server",
+			"rust-analyzer",
+			"typescript-language-server",
+			"js-debug-adapter",
+			"lua-language-server",
+			"pyright",
+			"biome",
+			"mypy",
+			"ruff",
+			"typos-lsp",
+			"swiftlint",
+			"rnix-lsp",
+			"nixfmt",
+			"nixpkgs-fmt",
+		},
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+}
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,11 +2,19 @@ return {
 	"neovim/nvim-lspconfig",
 	opts = {
 		inlay_hints = { enable = true },
+		servers = {
+			pyright = {},
+			tsserver = {},
+		},
 	},
 	config = function() end,
 	setup = {
 		type_lsp = function(_, opts)
 			require("typo-lsp").setup({ server = opts })
+		end,
+		tsserver = function(_, opts)
+			require("typescript").setup({ server = opts })
+			return true
 		end,
 	},
 }
